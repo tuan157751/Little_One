7ad9152841b9c814138b5825a49724be10f5e6e8
tuan157751 <88917565+tuan157751@users.noreply.github.com>
2024-06-25 23:02:48 +0700

    Add files via upload

---
+++ Little_one/Little_one/Camera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Little_one;
+
+namespace Little_one
+{
+    public class Camera
+    {
+        public Size screenSize;
+        public PointF cameraPosition;
+
+        public Camera(Size screenSize)
+        {
+            this.screenSize = screenSize;
+            this.cameraPosition = new PointF(0, 0);
+        }
+
+        public void Update(PointF playerPosition)
+        {
+            cameraPosition = new PointF ( playerPosition.X -screenSize.Width/2 ,   playerPosition.Y - screenSize.Height / 2);
+            
+        }
+
+        public PointF ApplyTransform(PointF position)
+        {
+            float x = position.X - cameraPosition.X;
+            float y = position.Y - cameraPosition.Y;
+            return new PointF(x,y);
+        }
+    }
+}
